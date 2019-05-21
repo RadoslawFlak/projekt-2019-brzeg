e645ea0bc36ac9008db9678c5d2a1af374a1cb6e
RadFla <radfla@st.amu.edu.pl>
2019-05-21 21:23:37 +0200

    Przelicznik walut na kursy z dnia 21 maja 2019

---
+++ Konwerter walut.R
@@ -0,0 +1,22 @@
+# Program ma przeprowadzać konwersję walut wg. przelicznika na dzień 21 maja 2019.
+# Zakładamy, że program byłby skierowany przede wszystkim do polskich odbiorców, którzy chcieliby wymienić polskiego nominały na zagraniczne i odwrotnie
+# Wybraliśmy 14 najpopularniejszych przeliczników według strony kantory.pl
+#
+# Ponadto w poniższej legendzie w celu późniejszej optymalniejszej i szybszej użyliśmy dodatkowych komend. Realniej najlepiej byłoby to połączyć z bazą danych, która się aktualizuje co kilkanaście sekund.
+#
+# Legenda:
+# symbol/nazwa/kraj/wartość kupna/sprzedaży na dzień 21.05.19
+# AUD - dolar australijski  - Australia
+# BGN - lew bułgarski       - Bułgaria
+# HRK - kuna chorwacka      - Chorwacja
+# CZK - korona czeska       - Czechy
+# DKK - korona duńska       - Dania
+# CAD - dolar kanadyjski    - Kanada
+# NOK - korona norweska     - Norwegia
+# RON - lej rumuński        - Rumunia
+# CHF - frank szwajcarski   - Szwajcaria
+# SEK - korona szwedzka     - Szwecja
+# EUR - euro                - Unia Europejska (strefa euro)
+# USD - dolar amerykański   - USA
+# HUF - forint węgierski    - Węgry
+# GBP - funt brytyjski      - Wielka Brytania
